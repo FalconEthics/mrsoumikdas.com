2c61c69cd80ae3e0ac93303338da65caba0a5592
Soumik Das <mail2soumikdas@gmail.com>
2023-08-05 00:13:53 +0530

    seo work starts
    ~ moved the project to a seo friendly stack

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex h-screen justify-center items-center">
-      <h1 className="text-3xl">Site under construction!</h1>
-    </div>
-  );
-}
-
-export default App;
