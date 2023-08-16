9a0cc56037f24fe190422ff4b24795480d3531e4
Soumik Das <mail2soumikdas@gmail.com>
2023-08-17 00:10:03 +0530

    projects page done for mobile devices
    ~ projects page pc responsiveness remaining and seo tags fix remaining
    ~ keep the flow going on
    ~ after all pages done : ~ add animations, page transitions, cursor, jsdoc and web3 at last

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,10 @@
-'use client'
+"use client";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Link from "next/link";
 import { useState, useLayoutEffect } from "react";
+import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub";
+import { FaGithubAlt } from "@react-icons/all-files/fa/FaGithubAlt";
 //Frameworks
 import react from "../assets/projects/Rectangle 29.avif";
 import motion from "../assets/projects/Rectangle 30.avif";
@@ -28,11 +30,13 @@ import Contact from "../components/Contact";
 import keeper from "../assets/projects/Group 17.avif";
 import covid from "../assets/projects/Rectangle 22.avif";
 import oiram from "../assets/projects/Group 18.avif";
-import appy from "../assets/projects/Group 13.avif";
-import likey from "../assets/projects/Group 14.avif";
+import apply from "../assets/projects/Group 13.avif";
+import likely from "../assets/projects/Group 14.avif";
 import gpt from "../assets/projects/Rectangle 26.avif";
 import tri from "../assets/projects/Rectangle 27.avif";
 import calc from "../assets/projects/Rectangle 28.avif";
+//coming soon img
+import soon from "../assets/projects/Group 16.avif";
 
 const Projects = () => {
   const [width, setWidth] = useState(0);
@@ -73,127 +77,183 @@ const Projects = () => {
   const projects = [
     {
       img: keeper,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "Keeper-App",
+      desc: "a quick notes-keeping app with cloud save feature made with 	React & Firebase",
+      repo: "https://github.com/FalconEthics/keeper-app",
+      url: "https://the-keeper-app-fe.web.app/",
     },
     {
       img: covid,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "COVID-UN",
+      desc: "a one-stop solution for all covid-19 stats & updates made with React Native",
+      repo: "https://github.com/FalconEthics/COVID-UN",
+      url: "https://github.com/FalconEthics/COVID-UN/releases/download/v-1.0.0/COVID-UN.apk",
     },
     {
       img: oiram,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "Oiram",
+      desc: "a browser-based survival game inspired by Mario that runs with the help of p5.js",
+      repo: "https://github.com/FalconEthics/OIRAM",
+      url: "https://oiram-falconethics.vercel.app/",
     },
     {
-      img: appy,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      img: apply,
+      title: "Appy3D",
+      desc: "a 3D website that features stunning animations that activate as the user scrolls!",
+      repo: "https://github.com/FalconEthics/Appy3d",
+      url: "https://appy3d-ooesozt5u-falconethics.vercel.app/",
     },
     {
-      img: likey,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      img: likely,
+      title: "Likely",
+      desc: "A social media platform frontend inspired from Twitter and Insta made with Vue.js",
+      repo: "https://github.com/FalconEthics/Likely",
+      url: "https://likey-falconethics.vercel.app/",
     },
     {
       img: gpt,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "Mr. GPT",
+      desc: "a Discord bot that uses OpenAI's GPT 3 learning model to generate text responses.",
+      repo: "https://github.com/FalconEthics/Mr-GPT",
     },
     {
       img: tri,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "Trinetro",
+      desc: "a single RPAS/Drone that can be used on Air, Water and Land for various purposes.",
+      repo: "https://github.com/FalconEthics/Trinetro-Drone",
     },
     {
       img: calc,
-      title: '',
-      desc: '',
-      repo: '',
-      url: ''
+      title: "CalcAsm",
+      desc: "a simple calculator that can perform basic arithmetic operations written intel - 8086 Assembly Language",
+      repo: "https://github.com/FalconEthics/CALCASM",
     },
-  ]
+  ];
 
   return (
     <>
       <div className="flex min-h-screen flex-col items-center w-full">
         <Contact />
         <Nav />
-        <div className="relative flex flex-col md:flex-row-reverse space-y-[8%] py-[8%] md:justify-between md:items-center md:space-y-[0%] md:py-[3%] w-full px-10 md:px-[16%]">
-          <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-500 text-center">Preferred Tech Stacks</h1>
-          <div className="flex flex-row w-full justify-between">
+        <div className="relative flex flex-col md:flex-row-reverse space-y-[8%] pt-[8%] md:justify-between md:items-center md:space-y-[0%] md:py-[3%] w-full">
+          <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-500 text-center">
+            Preferred Tech Stacks
+          </h1>
+          <div className="flex flex-row w-full justify-between px-10 md:px-[16%]">
             <div className="flex flex-col justify-start space-y-4 items-center w-1/3">
-              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">Frameworks</p>
+              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">
+                Frameworks
+              </p>
               <div className="flex flex-col justify-start items-center space-y-4 w-full">
-                {
-                  skills.Frameworks.map((item, index) => {
-                    return (
-                      <Image src={item} key={index} className="w-[60%]" />
-                    )
-                  })
-                }
+                {skills.Frameworks.map((item, index) => {
+                  return <Image src={item} key={index} className="w-[60%]" />;
+                })}
               </div>
             </div>
             <div className="flex flex-col justify-start space-y-4 items-center w-1/3">
-              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">Tools</p>
+              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">
+                Tools
+              </p>
               <div className="flex flex-col justify-start items-center space-y-4 w-full">
-                {
-                  skills.Tools.map((item, index) => {
-                    return width < 720 ? (
-                      index < 5 &&
+                {skills.Tools.map((item, index) => {
+                  return width < 720 ? (
+                    index < 5 && (
                       <Image src={item} key={index} className="w-[60%]" />
                     )
-                      : (
-                        <Image src={item} key={index} className="w-[60%]" />
-                      )
-                  })
-                }
+                  ) : (
+                    <Image src={item} key={index} className="w-[60%]" />
+                  );
+                })}
               </div>
             </div>
             <div className="flex flex-col justify-start space-y-4 items-center w-1/3">
-              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">Languages</p>
+              <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white text-center">
+                Languages
+              </p>
               <div className="flex flex-col justify-start items-center space-y-4 w-full">
-                {
-                  skills.Languages.map((item, index) => {
-                    return (
-                      <Image src={item} key={index} className="w-[60%]" />
-                    )
-                  })
-                }
+                {skills.Languages.map((item, index) => {
+                  return <Image src={item} key={index} className="w-[60%]" />;
+                })}
               </div>
             </div>
           </div>
-          <div className="text-center">
-            <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-500 text-center inline">My Projects</h2>
-            <p className="inline font-bold text-transparent bg-clip-text bg-gradient-to-r text-3xl from-main to-secondary text-center">_</p>
+          <div className="text-center px-10 md:px-[16%]">
+            <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-500 text-center inline">
+              My Projects
+            </h2>
+            <p className="inline font-bold text-transparent bg-clip-text bg-gradient-to-r text-3xl from-main to-secondary text-center">
+              _
+            </p>
           </div>
-          <div>
-            <div>
-              {/*   <Image src={item} key={index} /> */}
-              {/* <div> */}
-              {/*   <h3></h3> */}
-              {/*   <p></p> */}
-              {/*   <div> */}
-              {/*     <Link href={""} /> */}
-              {/*     <Link href={""} /> */}
-              {/*   </div> */}
-              {/* </div> */}
-            </div>
+          <div className="w-full flex flex-col">
+            {projects.map((item, index) => {
+              return (
+                <div className="flex flex-col w-full">
+                  <Image src={item.img} key={index} alt="projects" />
+                  <div className={`flex flex-col px-10 md:px-[16%] py-[8%] space-y-2 ${index == projects.length - 1 && 'pb-0' }`}>
+                    <h3 className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-main from-0% via-secondary via-10% to-white to-70% text-2xl">
+                      {item.title}
+                    </h3>
+                    <p>{item.desc}</p>
+                    <div className="flex flex-row items-center justify-start space-x-4">
+                      <Link
+                        href={item.repo}
+                        className={
+                          item.url
+                            ? "bg-black w-fit flex flex-row justify-center items-center space-x-1 border border-white p-2 px-4 font-bold rounded-lg"
+                            : "w-fit flex flex-row justify-center items-center space-x-1 bg-gradient-to-r from-main to-secondary p-2 px-4 font-bold rounded-lg"
+                        }
+                      >
+                        <AiFillGithub />
+                        <p
+                          className={
+                            item.url
+                              ? "text-transparent bg-clip-text bg-gradient-to-r from-main via-secondary to-white"
+                              : "text-white"
+                          }
+                        >
+                          REPOSITORY
+                        </p>
+                      </Link>
+                      {item.url && (
+                        <Link
+                          href={item.url}
+                          className="w-fit flex flex-row justify-center items-center space-x-1 bg-gradient-to-r from-main to-secondary p-2 px-4 font-bold rounded-lg"
+                        >
+                          <p className="text-white">
+                            {item.img == keeper || item.img == likely
+                              ? "OPEN WEB-APP"
+                              : item.img == covid
+                                ? "DOWNLOAD APK"
+                                : item.img == oiram
+                                  ? "PLAY GAME!"
+                                  : item.img == apply
+                                    ? "OPEN WEBSITE"
+                                    : ""}
+                          </p>
+                        </Link>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex flex-col justify-center items-center px-10 md:px-[16%] pb-[8%] space-y-6 bg-dark w-full">
+            <Image src={soon} alt="soon" className="w-[80%]"/>
+            <p className="text-sm text-dim text-center">
+              I am still working on some of the projects and I don't want to
+              present any unfinished works here. So, as soon as I complete them
+              I will be adding them here 👍 ~ till then you can check out my
+              GitHub profile for all the latest repos:
+            </p>
+            <Link
+              href={"https://github.com/FalconEthics"}
+              className="text-sm w-fit flex flex-row justify-center items-center space-x-1 bg-gradient-to-r from-main to-secondary p-2 px-4 font-bold rounded-lg"
+            >
+              <FaGithubAlt />
+              <p>Know more</p>
+            </Link>
           </div>
         </div>
         <Footer />
